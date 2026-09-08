cc5f56fd697d8465006f11af9a300dac857b180f
saad0417 <saadakhtar2222@gmail.com>
2026-09-09 02:00:10 +0500

    fix: remove trailing whitespace in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect } from 'react'; 
 import Lenis from 'lenis';
 import gsap from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
